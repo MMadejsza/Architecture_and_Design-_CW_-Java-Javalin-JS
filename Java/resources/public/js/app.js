b5ebb5fec70fac5fa8323838f326e7f492fccdd0
Maciej Madejsza <maciej.madejsza@gmail.com>
2024-04-19 13:31:32 +0100

    app.js updated

--- Java/resources/public/js/app.js
+++ Java/resources/public/js/app.js
@@ -2,13 +2,14 @@ document.addEventListener('DOMContentLoaded', function () {
 	const checkbox = document.getElementById('registerInput');
 	const registerForm = document.querySelector('.registerForm');
 	const deleteBtns = document.querySelectorAll('.delete');
-	console.log('btns', deleteBtns);
 
-	checkbox.addEventListener('change', function () {
-		if (checkbox.checked) {
-			registerForm.classList.add('active');
-		} else {
-			registerForm.classList.remove('active');
-		}
-	});
+	try {
+		checkbox.addEventListener('change', function () {
+			if (checkbox.checked) {
+				registerForm.classList.add('active');
+			} else {
+				registerForm.classList.remove('active');
+			}
+		});
+	} catch (error) {}
 });
