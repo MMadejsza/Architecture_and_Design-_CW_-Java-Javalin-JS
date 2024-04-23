3628113e07134f87f15650b2c33bb177dd1739ba
Maciej Madejsza <maciej.madejsza@gmail.com>
2024-04-23 22:15:52 +0100

    login javalin corrected

--- sad/src/main/java/sad/Main.java
+++ sad/src/main/java/sad/Main.java
@@ -54,7 +54,7 @@ public class Main {
       ctx -> {
         // Get the startDate and endDate query parameters from the frontend to use in yahoo stocks call
         String login = ctx.queryParam("name");
-        String password = ctx.queryParam("startDate");
+        String password = ctx.queryParam("password");
         // Sample data
 
         //Make the function selfcontaining, and when the stock is called produce the data
