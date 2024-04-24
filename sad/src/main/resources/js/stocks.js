9e8a9dbb1ecd6f568bfdeb345a56d5cd0ffa3af1
Maciej Madejsza <maciej.madejsza@gmail.com>
2024-04-24 18:49:47 +0100

    graphs edition issue solved

--- sad/src/main/resources/js/stocks.js
+++ sad/src/main/resources/js/stocks.js
@@ -4,15 +4,15 @@ document.addEventListener('DOMContentLoaded', function () {
 	const addInput = document.querySelector('.addInput');
 
 	// CHART generator util
-	const generateChart = (name, startDate = '2022-02-01', endDate = '2024-12-01') => {
+	const generateChart = (chartID, name, startDate = '2022-02-01', endDate = '2024-12-01') => {
 		// Catch defaultColor
 		const baseColor = getComputedStyle(document.documentElement).getPropertyValue(
 			'--defaultColor',
 		);
 		// Catch container
-		const ctx = document.getElementById('myChart');
+		const ctx = document.getElementById(chartID);
 		// Destroy existing chart if it exists
-		const existingChart = Chart.getChart('myChart');
+		const existingChart = Chart.getChart(chartID);
 		if (existingChart) existingChart.destroy();
 
 		// Generate label for each date in a range
@@ -44,12 +44,7 @@ document.addEventListener('DOMContentLoaded', function () {
 						datasets: [
 							{
 								label: name,
-								data: [
-									...values.close,
-									...values.high,
-									...values.low,
-									...values.open,
-								], // Use fetched data here
+								data: [...values.open], // Use fetched data here
 								fill: false,
 								borderColor: baseColor,
 								tension: 0.1,
@@ -84,34 +79,32 @@ document.addEventListener('DOMContentLoaded', function () {
 
 	// DATE INPUTS
 	const serveDateInputs = () => {
-		// catch all date inputs
-		const dateInputs = document.querySelectorAll("input[type='date']");
-
-		// loop over adding listener depending on edge date
-		[...dateInputs].forEach((input) => {
-			const chartLabelEl = input.closest('.graph-label');
-			const chartName = chartLabelEl.querySelector('h2').textContent;
-
-			if (input.className == 'startDateInput') {
-				input.addEventListener('input', (e) => {
-					const otherInputVal = chartLabelEl.querySelector('.endDateInput').value;
-					generateChart(
-						chartName,
-						e.target.value,
-						otherInputVal ? otherInputVal : undefined,
-					);
-				});
-			} else if (input.className == 'endDateInput') {
-				input.addEventListener('input', (e) => {
-					const otherInputVal = chartLabelEl.querySelector('.startDateInput').value;
-					generateChart(
-						chartName,
-						otherInputVal ? otherInputVal : undefined,
-						e.target.value,
-					);
-				});
-			}
-		});
+		// // catch all date inputs
+		// const dateInputs = document.querySelectorAll("input[type='date']");
+		// // loop over adding listener depending on edge date
+		// [...dateInputs].forEach((input) => {
+		// 	const chartLabelEl = input.closest('.graph-label');
+		// 	const chartName = chartLabelEl.querySelector('h2').textContent;
+		// 	if (input.className == 'startDateInput') {
+		// 		input.addEventListener('input', (e) => {
+		// 			const otherInputVal = chartLabelEl.querySelector('.endDateInput').value;
+		// 			generateChart(
+		// 				chartName,
+		// 				e.target.value,
+		// 				otherInputVal ? otherInputVal : undefined,
+		// 			);
+		// 		});
+		// 	} else if (input.className == 'endDateInput') {
+		// 		input.addEventListener('input', (e) => {
+		// 			const otherInputVal = chartLabelEl.querySelector('.startDateInput').value;
+		// 			generateChart(
+		// 				chartName,
+		// 				otherInputVal ? otherInputVal : undefined,
+		// 				e.target.value,
+		// 			);
+		// 		});
+		// 	}
+		// });
 	};
 
 	// APP LAYOUT CHANGE ON DELETION
@@ -129,32 +122,92 @@ document.addEventListener('DOMContentLoaded', function () {
 	const linkActions = () => {
 		changeGrid();
 		serveDeleteBtns();
-		serveDateInputs();
+		// serveDateInputs();
 	};
 
 	// ADD COMPANY
 	addInput.addEventListener('change', (e) => {
-		// Create graphBox element
-		const graphBox = document.createElement('div');
-		// Add matching in css class
-		graphBox.setAttribute('class', 'graphBox');
+		// Util value catching
+		const stockName = e.target.value;
 
-		// Create graphBox inner structure/content
-		graphBox.innerHTML = `<button class="delete">X</button>
-		<div class="graph">
-			<canvas id="myChart" width="100%">
-			</div>
-		<div class="graph-label">
-			<input class = "startDateInput" type="date" />
-			<h2 class = "graph-label-name">${e.target.value}</h2>
-			<input class = "endDateInput" type="date" />
-		</div>`;
+		// Util function for creating elements
+		const createEl = (el, attributes) => {
+			const element = document.createElement(el);
+			for (var key in attributes) {
+				element.setAttribute(key, attributes[key]);
+			}
+			return element;
+		};
+
+		// Create graphBox element
+		// const graphBox = document.createElement('div').setAttribute('class', 'graphBox');
+		const graphBox = createEl('div', {class: 'graphBox'});
+
+		//----------- Create graphBox inner structure/content---------------
+
+		// graphBox.innerHTML = `<button class="delete">X</button>
+		// <div class="graph">
+		// 	<canvas id="myChart" width="100%">
+		// </div>
+		// <div class="graph-label">
+		// 	<input class = "startDateInput" type="date" />
+		// 	<h2 class = "graph-label-name">${e.target.value}</h2>
+		// 	<input class = "endDateInput" type="date" />
+		// </div>`;
+
+		const button = createEl('button', {class: 'delete'});
+
+		const graph = createEl('div', {class: 'graph'});
+
+		// Unique Id for each chart for further actions on it
+		const canvas = createEl('canvas', {width: '100%', id: `myChart${stockName}`});
+
+		const graphLabel = createEl('div', {class: 'graph-label'});
+
+		const graphLabelName = createEl('h2', {class: 'graph-label-name'});
+		graphLabelName.innerText = stockName;
+
+		const inputStart = createEl('input', {type: 'date', class: 'startDateInput'});
+		inputStart.addEventListener('input', (e) => {
+			const otherInputVal = inputEnd.value;
+			generateChart(
+				`myChart${stockName}`,
+				stockName,
+				e.target.value,
+				otherInputVal ? otherInputVal : undefined,
+			);
+		});
+
+		// const inputEnd = document.createElement('div');
+		// setAttributes(inputEnd, {type: 'date', class: 'endDateInput'});
+		const inputEnd = createEl('input', {type: 'date', class: 'endDateInput'});
+		inputEnd.addEventListener('input', (e) => {
+			const otherInputVal = inputStart.value;
+			generateChart(
+				`myChart${stockName}`,
+				stockName,
+				otherInputVal ? otherInputVal : undefined,
+				e.target.value,
+			);
+		});
+
+		graphLabel.appendChild(inputStart);
+		graphLabel.appendChild(graphLabelName);
+		graphLabel.appendChild(inputEnd);
+
+		graph.appendChild(canvas);
+
+		graphBox.appendChild(button);
+		graphBox.appendChild(graph);
+		graphBox.appendChild(graphLabel);
+
+		//----------- END Create graphBox inner structure/content---------------
 
 		// Insert as first child
 		portfolio.insertBefore(graphBox, portfolio.firstChild);
 
 		// Generate Initial Graph
-		generateChart(e.target.value);
+		generateChart(`myChart${stockName}`, stockName);
 
 		// Linking all buttons and inputs
 		linkActions();
