d915f680992c55b8c901922c00e7bf40ee535d9c
Maciej Madejsza <maciej.madejsza@gmail.com>
2024-05-14 17:56:12 +0100

    portfolio commented

--- sad/src/main/resources/js/portfolio.js
+++ sad/src/main/resources/js/portfolio.js
@@ -1,19 +1,28 @@
 document.addEventListener('DOMContentLoaded', function () {
+	// Start of refreshing the wallet view
 	refreshWallet();
+
+	// Catch desired elements and values
 	const portfolio = JSON.parse(getCookie('portfolio'));
-	console.log(`portfolio ${portfolio}`);
 	const table = document.querySelector('table');
 
-	const updateStocks = async () => {
+	// Util function to get array of portfolio with stocks current prices
+	const populatePortfolio = async () => {
 		const portfolioWithValues = [];
+		// Promise all to wait for all promises (fetches)
 		await Promise.all(
+			// Map after fetched from cookies portfolio with just names
 			portfolio.map(async (portfolioStock) => {
+				// Fetch real data for each
 				const response = await fetch(`/fetchedStocks?name=${portfolioStock.name}`);
 				const data = await response.json();
+				// Get current price as last value for updated daily stocks
 				const allValues = data.chart.result[0].indicators.quote[0].open;
 				const currentPrice = allValues[allValues.length - 1]
 					? allValues[allValues.length - 1]
-					: allValues[allValues.length - 2];
+					: // There may be the case where todays date is populated but value not updated yet so in this case get yesterday's one
+					  allValues[allValues.length - 2];
+				// We store stocks names as objects so add extra property with current price
 				portfolioStock.currentPrice = currentPrice;
 				portfolioWithValues.push(portfolioStock);
 			}),
@@ -21,29 +30,37 @@ document.addEventListener('DOMContentLoaded', function () {
 		return portfolioWithValues;
 	};
 
+	// altered data format for the table (to have name, current price, hold amount and total value)
+	// Main function to calculate and generate portfolio table values
 	const generateTable = async () => {
-		const portfolioWithValues = await updateStocks();
-		console.log(portfolioWithValues);
+		// Get portfolio with prices
+		const portfolioWithValues = await populatePortfolio();
+
+		// Sort portfolio for total value descending
 		portfolioWithValues.sort(
 			(stock1, stock2) =>
 				stock2.amount * stock2.currentPrice - stock1.amount * stock1.currentPrice,
 		);
-		portfolioWithValues.forEach((portfolioStock) => {
-			const tr = document.createElement('tr');
 
+		// For each stock create and populate row with cells
+		portfolioWithValues.forEach((portfolioStock) => {
+			// ROW
+			const tr = document.createElement('tr');
+			// NAME
 			const td1 = document.createElement('td');
 			td1.innerText = portfolioStock.name;
-
+			// AMOUNT
 			const td2 = document.createElement('td');
 			td2.innerText = portfolioStock.amount.toFixed(4);
-
+			// PRICE
 			const td3 = document.createElement('td');
 			td3.innerText = portfolioStock.currentPrice.toFixed(4);
-
+			// TOTAL VALUE
 			const td4 = document.createElement('td');
 			const value = (portfolioStock.amount * portfolioStock.currentPrice).toFixed(4);
 			td4.innerText = value;
 
+			// ASSEMBLE
 			tr.appendChild(td1);
 			tr.appendChild(td2);
 			tr.appendChild(td3);
@@ -52,10 +69,12 @@ document.addEventListener('DOMContentLoaded', function () {
 			table.appendChild(tr);
 		});
 
+		// Additionally for each record generate chart to allow performing actions on purchased stocks. Kept inside this table function due to await 'portfolioWithValues' feature
 		portfolioWithValues.forEach((stock) => {
 			inputAddFunction('', stock.name);
 		});
 	};
 
+	// Call main function
 	generateTable();
 });
